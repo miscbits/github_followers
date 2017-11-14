eb28118b70149848946e2c0d3be84a3b6d3ae745
miscbits <willow6786@gmail.com>
2017-11-13 21:28:16 -0500

    passport install

---
+++ app/Http/Controllers/Api/GithubController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+
+class GithubController extends Controller
+{
+    public function search(Request $request) {
+    	
+    }
+}
