69348ee01d0110f99860fd49eb356fc9669f6bfb
miscbits <willow6786@gmail.com>
2017-11-14 00:51:21 -0500

    creating states for follower view loop

--- app/Http/Controllers/Api/GithubController.php
+++ app/Http/Controllers/Api/GithubController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers\API;
 
+use Cache;
+use Github;
 use Illuminate\Http\Request;
 
 class GithubController extends Controller
 {
     public function search(Request $request) {
-    	
+        $github_api = Github::api('user');
+        // Cache to remove redundant calls. bcrypt for faster cache response time
+        $user = [
+            "profile" => Cache::remember(bcrypt($request->input('username')), 5, function() {
+                $github_api->find($request->input('username'));
+            }),
+            "followers" => $github_api->followers($request->input('username'), 
+                ['per_page' => 50, 'page' => $request->input('page', 1)]),
+        ];
     }
 }
--- resources/assets/js/components/search-component.js
+++ resources/assets/js/components/search-component.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class GithubSearch extends Component {
+    constructor(props) {
+        super(props);
+
+        this.searchForUser = this.searchForUser.bind(this);
+    }
+
+    searchForUser() {
+        user = document.getElementById('username').value;
+
+        axios.get('/api/user',{
+            params: {username:user}
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+    }
+
     render() {
         return (
             <div className="container">
@@ -13,9 +32,9 @@ export default class GithubSearch extends Component {
                             <div className="panel-body">
                                 <div className="form-group">
                                     <label>Github Username</label>
-                                    <input className="form-control" type="text" name="username" placeholder="taylorotwell" />
+                                    <input className="form-control" type="text" name="username" id="username" placeholder="taylorotwell" />
                                 </div>
-                                <button id="github_search_submit_button" className="btn btn-primary"><i class="fa fa-search"></i> Search</button>
+                                <button id="github_search_submit_button" className="btn btn-primary" onClick={this.searchForUser}><i class="fa fa-search"></i> Search</button>
                             </div>
                         </div>
                     </div>
@@ -30,6 +49,8 @@ export default class GithubSearch extends Component {
             </div>
         );
     }
+
+
 }
 
 if (document.getElementById('github_search')) {
