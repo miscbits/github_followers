fec2191bd491d1a3058338421488ba26d526271d
miscbits <willow6786@gmail.com>
2017-11-14 21:20:22 -0500

    implemented more modules and got an mvp

--- app/Http/Controllers/Api/GithubController.php
+++ app/Http/Controllers/Api/GithubController.php
@@ -5,18 +5,17 @@ namespace App\Http\Controllers\API;
 use Cache;
 use Github;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class GithubController extends Controller
 {
     public function search(Request $request) {
-        $github_api = Github::api('user');
         // Cache to remove redundant calls. bcrypt for faster cache response time
         $user = [
-            "profile" => Cache::remember(bcrypt($request->input('username')), 5, function() {
-                $github_api->find($request->input('username'));
-            }),
-            "followers" => $github_api->followers($request->input('username'), 
+            "profile" => Github::api('user')->show($request->input('username')),
+            "followers" => Github::api('user')->followers($request->input('username'), 
                 ['per_page' => 50, 'page' => $request->input('page', 1)]),
         ];
+        return $user;
     }
 }
--- resources/assets/js/components/search-component.js
+++ resources/assets/js/components/search-component.js
@@ -5,23 +5,94 @@ export default class GithubSearch extends Component {
     constructor(props) {
         super(props);
 
+        this.nextPage = this.nextPage.bind(this);
         this.searchForUser = this.searchForUser.bind(this);
+
+        this.state = {
+            user: {
+                id : "",
+                gravatar_id : "",
+                username : "",
+                login : "",
+                name : "",
+                fullname : "",
+                location : "",
+                language : "",
+                type : "",
+                public_repo_count : "",
+                repos : "",
+                followers : 0,
+                followers_count : 0,
+                score : "",
+                created_at : "",
+                created : ""
+            },
+            followers : [],
+            page : 1,
+            last_page : true
+        }
     }
 
-    searchForUser() {
-        user = document.getElementById('username').value;
-
+    searchForUser(event) {
+        var self = this;
         axios.get('/api/user',{
-            params: {username:user}
+            params: {username:document.getElementById('username').value}
         }).then(function (response) {
             console.log(response);
+            self.setState({
+                user: response.data.profile,
+                followers: response.data.followers,
+                page : 1
+            });
+            if( self.state.page*50 >= self.state.user.followers ) {
+                self.setState({last_page : true});
+            } else {
+                self.setState({last_page : false});
+            }
         }).catch(function (error) {
             console.log(error);
         });
+    }
 
+    nextPage(event) {
+        var self = this;
+        axios.get('/api/user',{
+            params: {username:self.state.user.login,page:self.state.page+1}
+        }).then(function (response) {
+            console.log(response);
+            self.setState({
+                user: response.data.profile,
+                followers: response.data.followers,
+                page : self.state.page+1
+            });
+            if( self.state.page*50 >= self.state.user.followers ) {
+                self.setState({last_page : true});
+            } else {
+                self.setState({last_page : false});
+            }
+        }).catch(function (error) {
+            console.log(error);
+        });
     }
 
     render() {
+        const renderFollowers = this.state.followers.map((follower) => {
+          return (
+            <div className="col-md-6" key={follower.id}>
+                <div className="col-sm-2"><img className="img-responsive" src={follower.avatar_url}></img></div>
+                <div className="col-sm-4"><a href={follower.html_url}>{follower.login}</a></div>
+            </div>
+          );
+        });
+        
+        const isLastPage = this.state.last_page;
+        let page_button = null;
+        if (isLastPage) {
+            page_button = <LastPageButton />;
+        } else {
+            page_button = <NextPageButton onClick={this.nextPage} />;
+        }
+
         return (
             <div className="container">
                 <div className="row">
@@ -34,7 +105,14 @@ export default class GithubSearch extends Component {
                                     <label>Github Username</label>
                                     <input className="form-control" type="text" name="username" id="username" placeholder="taylorotwell" />
                                 </div>
-                                <button id="github_search_submit_button" className="btn btn-primary" onClick={this.searchForUser}><i class="fa fa-search"></i> Search</button>
+                                <button id="github_search_submit_button" className="btn btn-primary" onClick={this.searchForUser}><i className="fa fa-search"></i> Search</button>
+                                <div className="row">
+                                    <div className="col-sm-2"><img className="img-responsive" src={this.state.user.avatar_url}></img></div>
+                                    <div className="col-sm-4">
+                                        <h1><a href={this.state.user.html_url}>@{this.state.user.login}</a></h1>
+                                        <p>Followers: {this.state.user.followers}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -42,15 +120,31 @@ export default class GithubSearch extends Component {
 
                 <div className="row">
                     <div id="github_search_results" className="col-md-8 col-md-offset-2">
-                        
+                        <div className="panel panel-default">
+                            <div className="panel-body">
+                                {renderFollowers}
+                                <div className="pull-right">
+                                    {page_button}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         );
     }
+}
 
+function NextPageButton(props) {
+    return (
+        <button className="btn btn-info" onClick={props.onClick}>Get Next Page</button>
+    );
+}
 
+function LastPageButton(props) {
+    return (
+        <button className="btn btn-info disabled" onClick={props.onClick}>No More Data</button>
+    )
 }
 
 if (document.getElementById('github_search')) {
